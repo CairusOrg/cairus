3ad6458f04c9e3558609e3b8bcd5f491a1771d03
ferdousi@pdx.edu <ferdousi@pdx.edu>
2017-01-18 15:03:16 -0800

    added cairo_t implementation

---
+++ src/cairo_t.rs
@@ -0,0 +1,69 @@
+/*
+ * Cairus - a reimplementation of the cairo graphics library in Rust
+ *
+ * Copyright © 20XX CairusOrg
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it either under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation
+ * (the "LGPL") or, at your option, under the terms of the Mozilla
+ * Public License Version 2.0 (the "MPL"). If you do not alter this
+ * notice, a recipient may use your version of this file under either
+ * the MPL or the LGPL.
+ *
+ * You should have received a copy of the LGPL along with this library
+ * in the file LICENSE-LGPL-2_1; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335, USA
+ * You should have received a copy of the MPL along with this library
+ * in the file LICENSE-MPL-2_0
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY
+ * OF ANY KIND, either express or implied. See the LGPL or the MPL for
+ * the specific language governing rights and limitations.
+ *
+ * The Original Code is the cairus graphics library.
+ *
+ * Contributor(s):
+ *	Sara Ferdousi <ferdousi@pdx.edu>
+ *
+ */
+
+use cairo_status::cairo_status_t;
+
+struct cairo_array{
+    size: u64,
+    num_elements: u64,
+    element_size: u64,
+    elements: &char
+
+}
+
+pub struct cairo_t{
+
+    pub ref_count: u64,
+    pub cairo_status: cairo_status_t,
+    pub user_data_array: cairo_array
+
+}
+
+impl cairo_t{
+
+    fn cairo_reference(cr: &mut cairo_t) -> cairo_t{
+        cr.ref_count+=1; //increases reference count by one
+
+        return cr;
+    }
+
+    fn cairo_destroy(cr: &mut cairo_t){
+        if(cr.ref_count != 0){
+            cr.ref_count-=1;
+        }
+        //else need to free cr and associated resources. Not sure how to implement that
+    }
+
+}
