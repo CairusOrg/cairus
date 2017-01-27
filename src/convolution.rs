3ff8d4a93efda507b9ddda1b8438527d85e716f5
Bobby Eshleman <eshleman@pdx.edu>
2017-01-26 21:47:08 -0800

    Implemented polygon_angle

--- src/convolution.rs
+++ src/convolution.rs
@@ -1,6 +1,6 @@
 use std::f32;
 
-/// Returns the number of vertices in a polygon for apporiximating a pen tip of a certain width and
+/// Returns the number of vertices in a polygon for approximating a pen tip of a certain width and
 /// desired flatness.
 ///
 /// The lower the flatness, the more vertices the polygon will have.  This is because the more
@@ -21,12 +21,30 @@ fn polygon_vertices(width: f32, flatness: f32) -> i32 {
     }
 }
 
+struct Polygon {
+    empty: ()
+}
+
+impl Polygon {
+    // Returns sum of each angle of a polygon of `vertices`
+    //
+    // Example:  A triangle (3 vertices) angle sum is 180 degrees
+    // ```
+    // assert_eq!(angel_sum(3), 180); // true
+    // ```
+    fn angle_sum(vertices: i32) -> f32 {
+        180. + (180. * ((vertices as f32) - 3.))
+    }
+}
+
+
 /// ### References
 /// [1](https://keithp.com/~keithp/talks/cairo2003.pdf)
 
 #[cfg(test)]
 mod tests {
     use super::polygon_vertices;
+    use super::Polygon;
 
     #[test]
     fn test_polygon_edge_count1() {
@@ -47,5 +65,16 @@ mod tests {
         let width = 5.0;
         let flatness = 0.0001;
         assert_eq!(polygon_vertices(width, flatness), 352);
+    }
+
+    #[test]
+    fn test_polygon_angle(){
+        //let diameter = 2.0;
+        //let vertices = polygon_vertices(diameter, 0.1);
+        //let polygon = Polygon::new(diameter, vertices);
+        assert_eq!(Polygon::angle_sum(8), 1080.);
+        assert_eq!(Polygon::angle_sum(5), 540.);
+        assert_eq!(Polygon::angle_sum(3), 180.);
+
     }
 }
