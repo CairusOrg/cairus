110b963d50894c53f73ae50f50037a574660f6fe
ferdousi@pdx.edu <ferdousi@pdx.edu>
2017-02-11 16:09:39 -0800

    Added tests for first quadrant

--- src/decasteljau.rs
+++ src/decasteljau.rs
@@ -73,7 +73,7 @@ pub struct SplineKnots{
 impl SplineKnots{
 
     ///Creates a new SplineKnots with user defined points
-    pub fn create(a:Point, b:Point, c:Point, d:Point)->SplineKnots{
+    pub fn create(a: &Point, b: &Point, c: &Point, d: &Point)->SplineKnots{
         SplineKnots{
             a:Point::create(a.x, a.y),
             b:Point::create(b.x, b.y),
@@ -154,9 +154,11 @@ mod tests{
     use::decasteljau::Point;
     use::decasteljau::SplineKnots;
     use::decasteljau::DeCasteljauPoints;
+    use::decasteljau::lerp_half;
 
+    ///tests in Quadrant I
     #[test]
-    fn test_create_splineknots(){
+    fn test_splineknots_positive(){
         let mut q1 = Point::create(0.,0.);
         let q2 = Point::create(1., 2.);
         let q3 = Point::create(1.5, 2.4);
@@ -167,8 +169,8 @@ mod tests{
         let q7 = Point::create(1.9, 2.4);
         let q8 = Point::create(2.7, 3.3);
 
-        let mut r1 = SplineKnots::create(q1, q2, q3, q4);
-        let mut r2 = SplineKnots::create(q5, q6, q7, q8);
+        let mut r1 = SplineKnots::create(&q1, &q2, &q3, &q4);
+        let mut r2 = SplineKnots::create(&q5, &q6, &q7, &q8);
 
         assert_eq!(r1.a.x, 0.0);
         assert_eq!(r1.c.y, 2.4);
@@ -177,28 +179,30 @@ mod tests{
     }
 
     #[test]
-    fn test_splineknots_negative(){
-        let mut q1 = Point::create(0.,0.);
-        let q2 = Point::create(-1., -2.);
-        let q3 = Point::create(-1.5, -2.4);
-        let q4 = Point::create(2.6, 3.3);
+    fn test_lerp_half(){
+        let mut p1 = Point::create(0.,0.);
+        let mut p2 = Point::create(1., 2.);
+        let mut p3 = Point::create(1.5, 2.4);
+        let mut p4 = Point::create(2.6, 3.3);
 
-        let q5 = Point::create(0.,1.);
-        let q6 = Point::create(2., 2.);
-        let q7 = Point::create(1.9, 2.4);
-        let q8 = Point::create(-2.7, -3.3);
+        let p5 = Point::create(0.,1.);
+        let p6 = Point::create(2., 2.);
+        let p7 = Point::create(1.9, 2.4);
+        let p8 = Point::create(2.7, 3.3);
 
-        let mut r1 = SplineKnots::create(q1, q2, q3, q4);
-        let mut r2 = SplineKnots::create(q5, q6, q7, q8);
+        let mut s1 = SplineKnots::create(&p1, &p2, &p3, &p4);
+        let mut s2 = SplineKnots::create(&p5, &p6, &p7, &p8);
 
-        assert_eq!(r1.a.x, 0.0);
-        assert_eq!(r1.c.y, -2.4);
-        assert_eq!(r2.d.x, -2.7);
-        assert_eq!(r2.d.y, -3.3);
+        let mut d1 = DeCasteljauPoints::create();
+
+        let mut a1 = Point::origin();
+        a1 = lerp_half(&p7, &p8);
+        assert_eq!(a1.x, 2.3);
+        assert_eq!(a1.y, 2.85);
     }
 
     #[test]
-    fn test_create_spline(){
+    fn test_create_spline_positive(){
         let p1 = Point::create(0.,0.);
         let p2 = Point::create(1., 2.);
         let p3 = Point::create(1.5, 2.4);
@@ -209,8 +213,8 @@ mod tests{
         let p7 = Point::create(1.9, 2.4);
         let p8 = Point::create(2.7, 3.3);
 
-        let mut s1 = SplineKnots::create(p1, p2, p3, p4);
-        let mut s2 = SplineKnots::create(p5, p6, p7, p8);
+        let mut s1 = SplineKnots::create(&p1, &p2, &p3, &p4);
+        let mut s2 = SplineKnots::create(&p5, &p6, &p7, &p8);
 
         let mut d1 = DeCasteljauPoints::create();
 
@@ -219,6 +223,36 @@ mod tests{
         d1.create_spline(& mut s1,  & mut s2);
 
         assert_eq!(s2.a.x, d1.fin.x);
+        assert_eq!(s2.a.y, d1.fin.y);
+        assert_eq!(s2.b.x, d1.bccd.x);
+        assert_eq!(s2.b.y, d1.bccd.y);
+        assert_eq!(s2.c.x, d1.cd.x);
+        assert_eq!(s2.c.y, d1.cd.y);
+        assert_eq!(s2.d.x, p4.x);
+        assert_eq!(s2.d.y, p4.y);
+
+    }
+
+    ///tests in Quadrant II
+    #[test]
+    fn test_splineknots_negative(){
+        let q1 = Point::create(0.,0.);
+        let q2 = Point::create(-1., 2.);
+        let q3 = Point::create(-1.5, 2.4);
+        let q4 = Point::create(-2.6, 3.3);
+
+        let q5 = Point::create(0., 0.);
+        let q6 = Point::create(-2., 2.);
+        let q7 = Point::create(-1.9, 2.4);
+        let q8 = Point::create(-2.7, 3.3);
+
+        let mut r1 = SplineKnots::create(&q1, &q2, &q3, &q4);
+        let mut r2 = SplineKnots::create(&q5, &q6, &q7, &q8);
+
+        assert_eq!(r1.a.x, 0.0);
+        assert_eq!(r1.c.y, 2.4);
+        assert_eq!(r2.d.x, -2.7);
+        assert_eq!(r2.d.y, 3.3);
     }
 }
 
